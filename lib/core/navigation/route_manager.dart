ceecfd2fc45ada3dba5fc52d507c51ae4eca9c98
Andrii Kaidash <andriiflutter@gmail.com>
2025-01-27 00:03:19 +0200

    Update route manager

--- lib/core/navigation/route_manager.dart
+++ lib/core/navigation/route_manager.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:incode_test_task/modules/main_page/view/main_screen.dart';
 
 class RouteManager {
   RouteManager._();
 
   static final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
 
+  static final _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
+
   static GoRouter router = GoRouter(
     navigatorKey: _rootNavigatorKey,
+    initialLocation: MainScreen.routeName,
     debugLogDiagnostics: true,
-    routes: [],
+    routes: [
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (_, state, child) => MainScreen(
+          key: state.pageKey,
+          child: child,
+        ),
+        routes: [
+          // GoRoute(
+          //   path: HomeScreen.routeName,
+          // builder: (_, state) => HomeScreen()
+          // ),
+          // GoRoute(
+          //   path: ListScreen.routeName,
+          // builder: (_, state) => HomeScreen()
+          // ),
+        ],
+      ),
+    ],
   );
 }

a05f2b9f7d38d0a17a70b08dbe4b4e2c73061f61
Andrii Kaidash <andriiflutter@gmail.com>
2025-01-27 00:02:31 +0200

    Add styles class

---
+++ lib/resources/styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  Styles._();
+
+  static const selectedNavBar = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontSize: 12,
+  );
+
+  static const unselectedNavBar = TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.grey,
+    fontSize: 12,
+  );
+}

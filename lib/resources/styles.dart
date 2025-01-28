2f199d28af10be0beaf2f41925e81c67660fcfad
Andrii Kaidash <andriiflutter@gmail.com>
2025-01-29 01:48:38 +0200

    Add styles

--- lib/resources/styles.dart
+++ lib/resources/styles.dart
@@ -14,4 +14,28 @@ class Styles {
     color: Colors.grey,
     fontSize: 12,
   );
+
+  static const guessNumber = TextStyle(
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+    fontSize: 16,
+  );
+
+  static const guessType = TextStyle(
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+    fontSize: 16,
+  );
+
+  static const houseTitle = TextStyle(
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+    fontSize: 12,
+  );
+
+  static const characterName = TextStyle(
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+    fontSize: 20,
+  );
 }
